b567472cf7e0f5076d75be6ae526fc6f85501d88
coderchrisdean <coderchrisdean@gmail.com>
2023-04-09 12:12:19 -0700

    refactored client/src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/client';
+import client from './utils/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
+  <ApolloProvider client={client}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
