bbccb2c93806e042dc9b33d1209aad8424972a18
coderchrisdean <coderchrisdean@gmail.com>
2023-04-09 10:25:51 -0700

    updated index.js in models

--- server/server.js
+++ server/server.js
@@ -1,38 +1,30 @@
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express"); // import apollo-server-express
-const path = require("path");
-const { typeDefs, resolvers } = require("./schemas"); // import typeDefs and resolver schemas module
-const db = require("./config/connection");
-// const routes = require('./routes');
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-//set up apollo server as middleware
-
+// ApolloServer setup
+// Import the ApolloServer class
+const { ApolloServer } = require("apollo-server-express");
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require("./schemas");
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => req.headers,
 });
 
-server.start().then(() => {
+//Express setup
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
   server.applyMiddleware({ app });
 
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
+  // DB Setup
+  const db = require("./config/connection");
 
-  // if we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-  }
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-
-  // connect to MongoDB and start the server
-  db.once('open', () => {
+  db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(
@@ -40,4 +32,7 @@ server.start().then(() => {
       );
     });
   });
-});
+};
+
+// Call the async function to start the server
+startApolloServer();
