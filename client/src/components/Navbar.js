da6db5db483e2c3d387db17cc60157829397c3fa
coderchrisdean <coderchrisdean@gmail.com>
2023-04-09 12:30:06 -0700

    refactored Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { gql } from 'graphql-tag';
+
 import SignUpForm from './SignupForm';
 import LoginForm from './LoginForm';
-
 import Auth from '../utils/auth';
 
 const AppNavbar = () => {
